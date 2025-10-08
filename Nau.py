8577a96dd24db80bb083020b703cc5d97182100d
AriadnaFrey <a2510746@institutmontilivi.cat>
2025-10-08 13:13:44 +0200

    Update Nau.py

--- Nau.py
+++ Nau.py
@@ -17,15 +17,15 @@ def main():
         op = input('Entra una opció: ')
         if op=='d':
             #sumar 1 a la variable posX
-            posX=posX+1
+            pass
         elif op=='a':
-            posX=posX+1
+            pass
             #restar 1 a la variable posX
         elif op=='w':
-            posY=posY+1
+            pass
             #sumar 1 a la variable posY
         elif op=='s':
-            posY=posY+1
+            pass
             #restar 1 a la variable posY
         elif op=='0':
             sortir=True
